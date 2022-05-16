52b688401264cfec8015a8faf3680abb760a36c3
Glitch (glitch-hello-website) <none>
2022-05-16 07:41:55 +0000

    🏎😺 Checkpoint ./script.mjs:260052/1080 ./index.html:260052/985

--- script.mjs
+++ script.mjs
@@ -9,18 +9,30 @@ function draw_template() {
 
   const title = input_title.value;
   const lines = title.split('\n');
-  const [x1,y1,x2,y2] = [40,370,460,440];
-  ctx.font = `bold ${textSize}px sans-serif`;
-  const {width} = ctx.measureText()
-  const width = Math.max(...lines.map(l=>l.length));
-  const textSize = Math.min((x2-x1)/width*2, (y2-y1)/lines.length);
-  ctx.fillStyle = '#0a2f5b';
+  const [x1,y1,x2,y2] = [40,340,460,450];
+  ctx.font = `bold 10px sans-serif`;
+  const width = Math.max(...lines.map(l=>ctx.measureText(l).width));
+  const textSize = Math.min((x2-x1)/width*10, (y2-y1)/lines.length);
   ctx.font = `bold ${textSize}px sans-serif`
+  const theight = ctx.measureText(lines[0]).actualBoundingBoxAscent;
+  const lineGap = 1.2;
+  const height = (lines.length*lineGap)*theight;
+  ctx.fillStyle = '#0a2f5b';
+  const rwidth = Math.max(...lines.map(l=>ctx.measureText(l).width));
   lines.forEach((line,i) => {
-    ctx.fillText(line,40,371+i*36);
+    ctx.fillText(line,x2-rwidth,(y1+y2)/2-height/2+(i*lineGap+1)*theight);
+  })
+  canvas.toBlob(blob => {
+    const u = URL.createObjectUrl('blob');
+    
   })
 }
 
 draw_template();
 
 input_title.addEventListener('input',draw_template);
+
+function download() {
+}
+
+canvas.addEventListener('click', download)
